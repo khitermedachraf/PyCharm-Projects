89285f56faa7e59c3f49121045d432a029c94e4f
khitermedachraf <khiter.med7@gmail.com>
2020-09-20 21:34:47 +0100

    make some code on How to list file names and file folder path in a specific folder

--- first project/main.py
+++ first project/main.py
@@ -2,15 +2,30 @@
 
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
 
+path = "C:\\Users\\TRETEC\\Desktop\\KHITER Med Achraf"
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def listDir(dir) :
+    fileNames = os.listdir(dir)
+    for fileName in fileNames :
+        print("File name : " + fileName)
+        print("Folder path : " + os.path.abspath(os.path.join(dir,fileName)) + "\n")
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('KHITER Med Achraf')
 
-# Set PyCharm help at https://www.jetbrains.com/help/pycharm/
+result = os.listdir(path)
+for x in result:
+    print(x)
+print("hhhhhhhhhhhhhhhhh\n\n")
+
+listDir(path )
